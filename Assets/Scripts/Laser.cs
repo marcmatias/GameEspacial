44171ff0764ceac51a956be48f78574fa39c14e1
marcmatias <marcmatias@me.com>
2018-08-23 14:44:01 -0300

    first commit

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Translate(0,speed*Time.deltaTime,0);
+
+		if (transform.position.y > 5.5f)
+		{
+			Destroy(this.gameObject);
+		}
+		
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		print("Enter");
+	}
+
+	private void OnTriggerStay2D(Collider2D other)
+	{
+		print("Stay");
+	}
+
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		print("Exit");
+	}
+}
---
+++ Assets/Scripts/NaveController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NaveController : MonoBehaviour {
+
+	public float speed;
+	public GameObject prefabLaser;
+	public GameObject prefabLaserVerde;
+
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+
+		Movimentacao();
+		Atirar();
+		AtirarVerde();
+		VerificarLimites();
+	}
+
+	void Movimentacao()
+	{
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+
+		transform.Translate(h * speed * Time.deltaTime, 0, 0);
+		transform.Translate(0, v * speed * Time.deltaTime, 0);
+	}
+
+	void Atirar()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Instantiate(prefabLaser, transform.position + new Vector3(0, 0.67f, 0), Quaternion.identity);
+		}
+	}
+
+	void AtirarVerde()
+	{
+		if (Input.GetMouseButtonDown(1))
+		{
+			Instantiate(prefabLaserVerde, transform.position + new Vector3(0, 0.67f, 0), Quaternion.identity);
+		}
+	}
+
+	void VerificarLimites()
+	{
+		// Lateral
+		if(transform.position.x > 8.2f){
+			transform.position = new Vector3(8.2f, transform.position.y, 0);
+		} else if (transform.position.x < -8.2f){
+			transform.position = new Vector3(-8.2f, transform.position.y, 0);
+		}
+		// Vertical
+		if (transform.position.y > 4.49f){
+			transform.position = new Vector3(transform.position.x, 4.49f, 0);
+		} else if (transform.position.y < -4.49f){
+			transform.position = new Vector3(transform.position.x, -4.49f, 0);
+		}
+	}
+}
